1c7f8b04ccc4c1a6e2d63ec3be762a3cbc03c1f4
glopesdev <glopesdev@users.noreply.github.com>
2022-04-14 12:20:46 +0100

    Add support for creating daemon connections

---
+++ Bonsai.Tinkerforge/CreateIPConnection.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.ComponentModel;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using Tinkerforge;
+
+namespace Bonsai.Tinkerforge
+{
+    [Combinator(MethodName = nameof(Generate))]
+    [WorkflowElementCategory(ElementCategory.Source)]
+    [Description("Creates a TCP/IP connection to the Brick Daemon.")]
+    public class CreateIPConnection
+    {
+        internal const string DefaultHostName = "localhost";
+        internal const int DefaultPort = 4223;
+
+        [Description("The DNS name of the Brick Daemon on which you intend to connect.")]
+        public string HostName { get; set; } = DefaultHostName;
+
+        [Description("The port number of the Brick Daemon on which you intend to connect.")]
+        public int Port { get; set; } = DefaultPort;
+
+        [Description("The optional secret used when establishing authenticated connections to Brick Daemon.")]
+        public string Secret { get; set; }
+
+        public IObservable<IPConnection> Generate()
+        {
+            return Observable.Create<IPConnection>(observer =>
+            {
+                var connection = new IPConnection();
+                observer.OnNext(connection);
+
+                var secret = Secret;
+                connection.Connect(HostName, Port);
+                if (!string.IsNullOrEmpty(secret))
+                {
+                    connection.Authenticate(secret);
+                }
+
+                return Disposable.Create(() => connection.Disconnect());
+            });
+        }
+    }
+}
