2e8c3b95010ab5f5ea2c42aa39227bb7a0943e97
glopesdev <glopesdev@users.noreply.github.com>
2022-04-14 12:21:16 +0100

    Add extension method to ensure dispose sequence

---
+++ Bonsai.Tinkerforge/ObservableExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Reactive;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using Tinkerforge;
+
+namespace Bonsai.Tinkerforge
+{
+    static class ObservableExtensions
+    {
+        public static IObservable<TResult> SelectStream<TResult>(this IObservable<IPConnection> source, Func<IPConnection, IObservable<TResult>> selector)
+        {
+            return Observable.Create<TResult>(observer =>
+            {
+                var sourceDisposable = new CompositeDisposable();
+                var connectionObserver = Observer.Create<IPConnection>(
+                    connection =>
+                    {
+                        var stream = selector(connection);
+                        sourceDisposable.Add(stream.SubscribeSafe(observer));
+                    });
+                return new CompositeDisposable(
+                    sourceDisposable,
+                    source.SubscribeSafe(connectionObserver));
+            });
+        }
+    }
+}
