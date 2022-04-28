05a9a02706545ebc823078c87010d38663712ccd
Andrew Erskine <a.erskine@neurogears.org>
2022-04-28 18:04:32 +0100

    Mapping for device identifier --> device name

---
+++ Bonsai.Tinkerforge/TinkerforgeHelpers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Tinkerforge
+{
+    public class TinkerforgeHelpers
+    {
+
+        public static class TinkerForgeNameLookup
+        {
+            public static Dictionary<int, string> Defaults = new Dictionary<int, string>
+            {
+                { 13,  "Master Brick"},
+                { 2131, "Ambient Light Bricklet 3.0"},
+                { 2147, "CO2 Bricklet 2.0" }
+            };
+        }
+
+    }
+}
