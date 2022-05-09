5bf459670bb14c3e15d29c151eec41f539c0b310
RoboDoig <erskine94@gmail.com>
2022-05-09 14:04:26 +0100

    Sound pressure bricklet

---
+++ Bonsai.Tinkerforge/IndustrialPTC.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.ComponentModel;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using Tinkerforge;
+
+namespace Bonsai.Tinkerforge
+{
+    internal class IndustrialPTC
+    {
+    }
+}
