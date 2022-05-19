90bfdce39ff61ce94666159aded50259837b91bb
RoboDoig <erskine94@gmail.com>
2022-05-19 17:28:56 +0100

    Refactor type conversion for Bricklet Uid

--- Bonsai.Tinkerforge/UidConverter.cs
+++ Bonsai.Tinkerforge/UidConverter.cs
@@ -2,24 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.ComponentModel;
-using Bonsai.Expressions;
-using Tinkerforge;
-using System.Threading;
-using System.Diagnostics;
 using System.Globalization;
+using Bonsai.Expressions;
+using System.Threading;
+using Tinkerforge;
 
 namespace Bonsai.Tinkerforge
 {
-    internal class BrickletDeviceNameConverter : TypeConverter
+    internal class UidConverter : StringConverter
     {
+        // Cache for discovered Brick / Bricklet devices
         public Dictionary<string, DeviceData> devices;
 
         public override bool GetStandardValuesSupported(ITypeDescriptorContext context)
-        {
-            return true;
-        }
-
-        public override bool GetStandardValuesExclusive(ITypeDescriptorContext context)
         {
             return true;
         }
@@ -29,31 +24,34 @@ namespace Bonsai.Tinkerforge
             return sourceType == typeof(string) || base.CanConvertFrom(context, sourceType);
         }
 
-        // If we are doing the type conversion in a drop down, we are converting between a data object (DeviceData) and 
-        // a representative string in the drop down. We need to define the to/from conversion so that we can have a human
-        // readable representation of the device in the drop down, and use that string to look up the correct
-        // DeviceData on selection in the editor
+        // From display string --> Uid string
         public override object ConvertFrom(ITypeDescriptorContext context, CultureInfo culture, object value)
         {
-            var casted = value as string;
-            return casted != null
-                ? devices[casted]
+            var text = value as string;
+            return text != null
+                ? text.Split(':')[1]
                 : base.ConvertFrom(context, culture, value);
         }
 
+        // From Uid string --> display string
         public override object ConvertTo(ITypeDescriptorContext context, CultureInfo culture, object value, Type destinationType)
         {
-            var casted = value as DeviceData;
-            return destinationType == typeof(string) && casted != null
-                ? casted.ToString()
+            if (devices == null) LookupDevices(context);
+            var text = value as string;
+            return destinationType == typeof(string) && text != null
+                ? devices[text].ToString()
                 : base.ConvertTo(context, culture, value, destinationType);
         }
 
-        /// <summary>
-        /// For Bricklets, we have to specify a UID to connect to. To get that, we need to know what IP connections are present,
-        /// and what UIDs are present on those connections
-        /// </summary>
         public override StandardValuesCollection GetStandardValues(ITypeDescriptorContext context)
+        {
+            // Get the currently available device names
+            return LookupDevices(context);
+        }
+
+        // For Bricklets, we have to specify a UID to connect to. To get that, we need to know what IP connections are present,
+        // and what UIDs are present on those connections
+        private StandardValuesCollection LookupDevices(ITypeDescriptorContext context)
         {
             if (context != null)
             {
@@ -66,7 +64,7 @@ namespace Bonsai.Tinkerforge
                                          let createIP = ExpressionBuilder.GetWorkflowElement(builder) as CreateIPConnection
                                          where createIP != null
                                          select new ConnectionID { HostName = createIP.HostName, Port = createIP.Port })
-                                         .Distinct().ToList();
+                                            .Distinct().ToList();
 
                     // For each IP connection, we search the connected devices and add it to the list - TODO test for multiple IP
                     devices = new Dictionary<string, DeviceData>();
@@ -74,7 +72,7 @@ namespace Bonsai.Tinkerforge
                     {
                         var ipcon = new IPConnection();
                         ipcon.EnumerateCallback += EnumerateConnection;
-                        ipcon.Connect(connectionID.HostName, connectionID.Port); 
+                        ipcon.Connect(connectionID.HostName, connectionID.Port);
                         ipcon.Enumerate();
                         /// N.B. GetStandardValues is called twice by Windows Forms. Once to check the dropdown list and then again
                         /// to check cursor position in the list. This is annoying here because it means the enumerate thread will
@@ -85,22 +83,20 @@ namespace Bonsai.Tinkerforge
                         ipcon.Disconnect();
                     }
                 }
-                //return new StandardValuesCollection(devices.Select(x => x.DeviceIdentifier).ToList());
                 return new StandardValuesCollection(devices.Values.ToList());
             }
 
             return base.GetStandardValues(context);
         }
 
+        // Callback for IPConnection.Enumerate() that adds devices to the device list
         private void EnumerateConnection(IPConnection sender, string uid, string connectedUid, char position,
             short[] hardwareVersion, short[] firmwareVersion, int deviceIdentifier, short enumerationType)
         {
-            // Callback for IPConnection.Enumerate() that adds devices to the device list
             DeviceData discoveredDevice = new DeviceData(uid, connectedUid, position, deviceIdentifier);
-            devices.Add(discoveredDevice.ToString(), discoveredDevice);
+            devices.Add(discoveredDevice.Uid, discoveredDevice);
         }
 
-        // TODO - move this to its own file if it needs to be reused across modules
         public struct ConnectionID
         {
             public string HostName;
