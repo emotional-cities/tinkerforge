49141cd0527dbfc3cacce2ba0ea8862b98d501d7
RoboDoig <erskine94@gmail.com>
2022-06-09 14:36:17 +0100

    Altitude and DateTime streams

---
+++ Bonsai.Tinkerforge/GPSV2Altitude.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.ComponentModel;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using Tinkerforge;
+
+namespace Bonsai.Tinkerforge
+{
+    [DefaultProperty(nameof(Period))]
+    [Description("Generates an altitude data stream from a GPSV2 device.")]
+    public class GPSV2Altitude : Combinator<BrickletGPSV2, GPSV2Altitude.AltitudeData>
+    {
+        [Description("Specifies the period between sample event callbacks. A value of zero disables event reporting.")]
+        public long Period { get; set; } = 1000;
+
+        public override IObservable<AltitudeData> Process(IObservable<BrickletGPSV2> source)
+        {
+            return source.SelectStream(device =>
+            {
+                device.SetAltitudeCallbackPeriod(Period);
+                return Observable.Create<AltitudeData>(observer =>
+                {
+                    BrickletGPSV2.AltitudeEventHandler handler = (sender, altitude, geoidalSeparation) =>
+                    {
+                        observer.OnNext(new AltitudeData(altitude, geoidalSeparation));
+                    };
+
+                    device.AltitudeCallback += handler;
+                    return Disposable.Create(() =>
+                    {
+                        try { device.SetAltitudeCallbackPeriod(0); }
+                        catch (NotConnectedException) { }
+                        device.AltitudeCallback -= handler;
+                    });
+                });
+            });
+        }
+
+        public struct AltitudeData
+        {
+            public int Altitude;
+            public int GeoidalSeparation;
+
+            public AltitudeData(int altitude, int geoidalSeparation)
+            {
+                Altitude = altitude;
+                GeoidalSeparation = geoidalSeparation;
+            }
+        }
+    }
+}
---
+++ Bonsai.Tinkerforge/GPSV2DateTime.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.ComponentModel;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using Tinkerforge;
+
+namespace Bonsai.Tinkerforge
+{
+    [DefaultProperty(nameof(Period))]
+    [Description("Generates a datetime data stream from a GPSV2 device.")]
+    public class GPSV2DateTime : Combinator<BrickletGPSV2, GPSV2DateTime.DateTimeData>
+    {
+        [Description("Specifies the period between sample event callbacks. A value of zero disables event reporting.")]
+        public long Period { get; set; } = 1000;
+
+        public override IObservable<DateTimeData> Process(IObservable<BrickletGPSV2> source)
+        {
+            return source.SelectStream(device =>
+            {
+                device.SetDateTimeCallbackPeriod(Period);
+                return Observable.Create<DateTimeData>(observer =>
+                {
+                    BrickletGPSV2.DateTimeEventHandler handler = (sender, date, time) =>
+                    {
+                        observer.OnNext(new DateTimeData(date, time));
+                    };
+
+                    device.DateTimeCallback += handler;
+                    return Disposable.Create(() =>
+                    {
+                        try { device.SetDateTimeCallbackPeriod(0); }
+                        catch (NotConnectedException) { }
+                        device.DateTimeCallback -= handler;
+                    });
+                });
+            });
+        }
+
+        public struct DateTimeData
+        {
+            public long Date;
+            public long Time;
+
+            public DateTimeData(long date, long time)
+            {
+                Date = date;
+                Time = time;
+            }
+        }
+    }
+}
